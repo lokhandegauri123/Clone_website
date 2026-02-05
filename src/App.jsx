83f9fe42a15392acdba7750ebbf2051e48d58c99
Gauri Lokhande <157399853+lokhandegauri123@users.noreply.github.com>
2026-02-05 17:35:22 +0530

    clone website

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import './app.scss'
+import Dock from './component/Dock'
+
+function App() {
+
+  return (
+    <>
+    <main className='app'>
+      <Dock/>
+    </main>
+    </>
+  )
+}
+
+export default App
